c37d741b8cdb6421c5a80a5766192a8d9536613d
chiranjeevi <sree.4de@gmail.com>
2024-08-27 21:30:12 +0530

    updated changes

--- Command-output.sh
+++ Command-output.sh
@@ -3,3 +3,15 @@
 DATE=$(date +%F)
 
 echo "Today's date: $DATE"
+
+# special variables
+
+echo "All variables passed to script: $@"
+echo "Number of variables passed: $#"
+echo "script name: $0"
+echo "current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "PID of the script executing now: $$"
+
+sleep 100 &
+echo "PID of the last background command: $!"
