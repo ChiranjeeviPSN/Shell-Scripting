298b59141d1562bd0522c87c68c464858cc4fc1c
chiranjeevi <sree.4de@gmail.com>
2024-08-28 10:58:48 +0530

    Validate function

--- 01-Hello-World.sh
+++ 01-Hello-World.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Hello World"
+VALIDATE $? "Command"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is...SUCCESS"
+    fi
+}
