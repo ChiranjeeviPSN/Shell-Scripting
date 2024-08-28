da9e98b9d132761fd370839ec5bfe054fdc11f76
chiranjeevi <sree.4de@gmail.com>
2024-08-28 11:48:28 +0530

    Changes function

--- 01-Hello-World.sh
+++ 01-Hello-World.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Hello World"
-VALIDATE $? "Command"
-
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 is...FAILED"
+        echo "$3 is...FAILED"
         exit 1
     else
-        echo "$2 is...SUCCESS"
+        echo "$3 is...SUCCESS"
     fi
 }
+
+echo "Hello World"
+VALIDATE $? "Command" "failed command"
